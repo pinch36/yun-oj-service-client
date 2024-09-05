6a2a3a7d2c20e90b6d5819f66e2a00f3681f4196
__yun <ylw167@qq.com>
2024-09-05 22:58:47 +0800

    微服务初始化

---
+++ src/main/java/com/yun/oj/service/client/YunOjServiceClientApplication.java
@@ -0,0 +1,13 @@
+package com.yun.oj.service.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunOjServiceClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YunOjServiceClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yun/oj/service/client/YunOjServiceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yun.oj.service.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YunOjServiceClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
